9f75c9631584dce8f5cef62ba4e5eabe636a3641
chaitu0461 <chaitanya.rcvs@gmail.com>
2026-09-25 00:37:01 +0530

    Refactored and added comments. Modified logic for selection sort

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -2,17 +2,34 @@ package sorting;
 
 import java.util.Arrays;
 
-public class InsertionSort {
+public class  InsertionSort {
     public static void main(String[] args) {
-        int[] nums={89,81,286,1276};
-       for(int i=0;i< nums.length;i++){
-           int j=i;
-           while (j>0 && nums[j-1]>nums[j]){
-               int temp=nums[j];
-               nums[j]=nums[j-1];
-               nums[j-1]=temp;
-               j--;
-           }
+      /*
+       * INSERTION SORT:
+       *
+       * Step 1: Start from the second element; treat the first as sorted.
+       *
+       * Step 2: Pick the current element as `key`.
+       *
+       * Step 3: Compare `key` with elements to its left.
+       *
+       * Step 4: Shift elements greater than `key` one position right.
+       *
+       * Step 5: Insert `key` into its correct position.
+       *
+       * Step 6: Repeat until the entire array is sorted.
+       */
+
+      int[] nums={89,81,286,1276,0,5};
+       for(int unsortedIndex=1;unsortedIndex<nums.length;unsortedIndex++){
+         int sortedIndex = unsortedIndex-1;
+         int key = nums[unsortedIndex];
+         while (sortedIndex>=0 && nums[sortedIndex]>key){
+           nums[sortedIndex+1]=nums[sortedIndex];
+           sortedIndex--;
+         }
+         nums[sortedIndex+1]=key;
+
        }
         Arrays.stream(nums).forEach(System.out::println);
     }
--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -5,16 +5,18 @@ import java.util.Arrays;
 public class SelectionSort {
     public static void main(String[] args) {
         int[] nums={89,81,286,1276};
-        for(int sortedIndex=0;sortedIndex<nums.length-1;sortedIndex++){
-            int minIndexInUnsortedArray=sortedIndex;
-            for(int unsortedIndex=sortedIndex+1;unsortedIndex<nums.length;unsortedIndex++){
+      // smallestNumberIndex represents the position where the next
+      // smallest element should be placed.
+        for(int smallestNumberIndex=0;smallestNumberIndex<nums.length-1;smallestNumberIndex++){
+            int minIndexInUnsortedArray=smallestNumberIndex;
+            for(int unsortedIndex=smallestNumberIndex+1;unsortedIndex<nums.length;unsortedIndex++){
                 if(nums[minIndexInUnsortedArray]>nums[unsortedIndex])
                     minIndexInUnsortedArray=unsortedIndex;
 
             }
-            if(nums[minIndexInUnsortedArray]<nums[sortedIndex]){
-                int temp=nums[sortedIndex];
-                nums[sortedIndex]=nums[minIndexInUnsortedArray];
+            if(nums[minIndexInUnsortedArray]<nums[smallestNumberIndex]){
+                int temp=nums[smallestNumberIndex];
+                nums[smallestNumberIndex]=nums[minIndexInUnsortedArray];
                 nums[minIndexInUnsortedArray]=temp;
             }
         }
